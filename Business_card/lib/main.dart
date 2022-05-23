daef57303857617e6849c38c370f37f93a233863
Hari25483 <haricodmw@gmail.com>
2022-05-24 00:50:01 +0530

    Update main.dart

--- Business_card/lib/main.dart
+++ Business_card/lib/main.dart
@@ -18,10 +18,11 @@ class MyApp extends StatelessWidget {
                   "images/270042121_4518560931604207_3691290218835539511_n.jpeg"),
             ),
             Text(
-              'Hariharan Raveenthiran',
+              'Hariharan',
               style: TextStyle(
                 fontSize: 40.00,
                 color: Colors.white,
+                letterSpacing: 2.5,
                 fontWeight: FontWeight.bold,
                 fontFamily: 'Pacifico',
               ),
@@ -34,6 +35,52 @@ class MyApp extends StatelessWidget {
                   fontSize: 25.00,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold),
+            ),
+            Card(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin:
+                    EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.00),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.00,
+                    ),
+                    Text(
+                      "+00 94  771525093",
+                      style: TextStyle(
+                          fontSize: 20.00,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal.shade900),
+                    )
+                  ],
+                )),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.00),
+              margin: EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.00),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.00,
+                  ),
+                  Text(
+                    "haricodmw@gmail.com",
+                    style: TextStyle(
+                        fontSize: 20.00,
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900),
+                  )
+                ],
+              ),
             )
           ],
         )),
