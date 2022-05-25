cd26f887ac74bde93477806bbdaf652b776bf97b
Hari25483 <haricodmw@gmail.com>
2022-05-26 02:19:03 +0530

    Create a stateful widget

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -1,52 +1,32 @@
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   return runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.blue.shade500,
-//         appBar: AppBar(
-//           title: Text("Ask Me Anything"),
-//           backgroundColor: Colors.blue.shade900,
-//         ),
-//         body: Magic_Ball(),
-//       ),
-//     ),
-//   );
-// }
-//
-// class Magic_Ball extends StatefulWidget {
-//   const Magic_Ball({Key? key}) : super(key: key);
-//
-//   @override
-//   State<Magic_Ball> createState() => _Magic_BallState();
-// }
-//
-// class _Magic_BallState extends State<Magic_Ball> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Image.asset("images/ball1.png"),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(home: BallPage()),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade500,
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Magic_Ball(),
+      ),
+    ),
   );
 }
 
-class BallPage extends StatelessWidget {
+class Magic_Ball extends StatefulWidget {
+  const Magic_Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Magic_Ball> createState() => _Magic_BallState();
+}
+
+class _Magic_BallState extends State<Magic_Ball> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue.shade500,
-      appBar: AppBar(
-        title: Text("Ask Me Anything"),
-      ),
-      body: Container(),
+    return Center(
+      child: Image.asset("images/ball1.png"),
     );
   }
 }
