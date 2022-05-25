f3bbe7e339922eb18393459c93fda61184a77033
Hari25483 <haricodmw@gmail.com>
2022-05-26 02:18:36 +0530

    Create a stateless widget

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -1,7 +1,52 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   return runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.blue.shade500,
+//         appBar: AppBar(
+//           title: Text("Ask Me Anything"),
+//           backgroundColor: Colors.blue.shade900,
+//         ),
+//         body: Magic_Ball(),
+//       ),
+//     ),
+//   );
+// }
+//
+// class Magic_Ball extends StatefulWidget {
+//   const Magic_Ball({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Magic_Ball> createState() => _Magic_BallState();
+// }
+//
+// class _Magic_BallState extends State<Magic_Ball> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Image.asset("images/ball1.png"),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(home: BallPage()),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade500,
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
       ),
+      body: Container(),
     );
+  }
+}
