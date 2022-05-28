ad4aaf25bbe1ef63fb13f4bc29ef36cf1fffacc8
Hari25483 <haricodmw@gmail.com>
2022-05-28 21:50:01 +0530

    Add alert to indicate quiz end

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -48,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
         Alert(
           context: context,
           type: AlertType.success,
-          title: "RFLUTTER ALERT",
+          title: "Congratulations",
           desc: "You have tried all the questions.",
           buttons: [
             DialogButton(
