d43a4d894da449bc8fdb43c7961392f544f1bf80
Hari25483 <haricodmw@gmail.com>
2022-05-31 01:58:23 +0530

    Edit custom widget

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.00;
 const reusableWidgetColor = Color(0xFF1D1E33);
@@ -17,7 +18,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xFF0A0E21),
-          title: Text("BMI Calculator"),
+          title: const Text("BMI Calculator"),
         ),
         body: Column(
           children: [
@@ -25,22 +26,30 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Row(
               children: [
                 Expanded(
-                  child: Reusablewidget(colour: reusableWidgetColor),
+                  child: Reusablewidget(
+                    colour: reusableWidgetColor,
+                    cardChild: IconWidget(),
+                  ),
                 ),
-                Expanded(child: Reusablewidget(colour: reusableWidgetColor))
+                Expanded(
+                    child: Reusablewidget(
+                        cardChild: Column(), colour: reusableWidgetColor))
               ],
             )),
             Expanded(
-              child: Reusablewidget(colour: reusableWidgetColor),
+              child: Reusablewidget(
+                  cardChild: Column(), colour: reusableWidgetColor),
             ),
             Expanded(
                 child: Row(
               children: [
                 Expanded(
-                  child: Reusablewidget(colour: reusableWidgetColor),
+                  child: Reusablewidget(
+                      cardChild: Column(), colour: reusableWidgetColor),
                 ),
                 Expanded(
                   child: Reusablewidget(
+                    cardChild: Column(),
                     colour: reusableWidgetColor,
                   ),
                 )
@@ -56,16 +65,46 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Reusablewidget extends StatelessWidget {
-  Reusablewidget({required this.colour});
-  Color colour;
+class IconWidget extends StatelessWidget {
+  const IconWidget({
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(
+            fontSize: 18.00,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Reusablewidget extends StatelessWidget {
+  const Reusablewidget({required this.colour, required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration:
           BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
+      child: cardChild,
     );
   }
 }
