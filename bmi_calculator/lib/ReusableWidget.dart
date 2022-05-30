2e40700210a6f54b4d4b03fd000fa747dfe8f080
Hari25483 <haricodmw@gmail.com>
2022-05-31 02:21:34 +0530

    Refactor Reusable Widget

---
+++ bmi_calculator/lib/ReusableWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Reusablewidget extends StatelessWidget {
+  Reusablewidget({required this.colour, required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration:
+          BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
+      child: cardChild,
+    );
+  }
+}
