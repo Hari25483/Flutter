071d794da8c067d1c7c35034dcbdd631429dea41
Hari25483 <haricodmw@gmail.com>
2022-05-31 23:42:59 +0530

    Use Ternary operator to minimize gender code

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi_calculator/ReusableWidget.dart';
 import 'package:bmi_calculator/IconWidget.dart';
 
-enum Gender { male, female }
+enum Gender { male, female, nothing }
 
 const bottomContainerHeight = 80.00;
 const activeWidgetColor = Color(0xFF1D1E33);
@@ -20,24 +20,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Color maleCardColor = inactiveWidgetColor;
   Color femaleCardColor = inactiveWidgetColor;
-
-  void updateColor(Gender gender) {
-    if (gender == Gender.male) {
-      if (maleCardColor == inactiveWidgetColor) {
-        maleCardColor = activeWidgetColor;
-        femaleCardColor = inactiveWidgetColor;
-      } else {
-        maleCardColor = inactiveWidgetColor;
-      }
-    } else if (gender == Gender.female) {
-      if (femaleCardColor == inactiveWidgetColor) {
-        femaleCardColor = activeWidgetColor;
-        maleCardColor = inactiveWidgetColor;
-      } else {
-        femaleCardColor = inactiveWidgetColor;
-      }
-    }
-  }
+  Gender selectedGender = Gender.nothing;
 
   @override
   Widget build(BuildContext context) {
@@ -55,12 +38,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updateColor(Gender.male);
+                        selectedGender = Gender.male;
                         print("Male card was pressed!");
                       });
                     },
                     child: Reusablewidget(
-                      colour: maleCardColor,
+                      colour: selectedGender == Gender.male
+                          ? activeWidgetColor
+                          : inactiveWidgetColor,
                       cardChild: IconWidget(
                           icon: FontAwesomeIcons.mars, label: 'MALE'),
                     ),
@@ -70,14 +55,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: GestureDetector(
                   onTap: () {
                     setState(() {
-                      updateColor(Gender.female);
+                      selectedGender = Gender.female;
                       print("Female card was pressed!");
                     });
                   },
                   child: Reusablewidget(
                       cardChild: IconWidget(
                           icon: FontAwesomeIcons.venus, label: 'FEMALE'),
-                      colour: femaleCardColor),
+                      colour: selectedGender == Gender.female
+                          ? activeWidgetColor
+                          : inactiveWidgetColor),
                 ))
               ],
             )),
