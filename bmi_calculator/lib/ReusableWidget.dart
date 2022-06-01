45ac7256b2552bd2c23a06b2f98088fe2a5fd86f
Hari25483 <haricodmw@gmail.com>
2022-06-02 00:33:21 +0530

    Update ReusableWidget.dart

--- bmi_calculator/lib/ReusableWidget.dart
+++ bmi_calculator/lib/ReusableWidget.dart
@@ -6,11 +6,13 @@ class Reusablewidget extends StatelessWidget {
   final Widget cardChild;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration:
-          BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
-      child: cardChild,
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            color: colour, borderRadius: BorderRadius.circular(10)),
+        child: cardChild,
+      ),
     );
   }
 }
