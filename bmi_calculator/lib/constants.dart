eef32befe453fab24aaa66e5fc77160d1a24a3b5
Hari25483 <haricodmw@gmail.com>
2022-06-02 00:33:26 +0530

    Create constants.dart

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kbottomContainerHeight = 80.00;
+const kactiveWidgetColor = Color(0xFF1D1E33);
+const kinactiveWidgetColor = Color(0xFF111328);
+const kbottomContainerColor = Color(0xFFEB1555);
+
+const LabelTextStyle = TextStyle(
+  fontSize: 18.00,
+  color: Color(0xFF8D8E98),
+);
+
+const knumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w800);
