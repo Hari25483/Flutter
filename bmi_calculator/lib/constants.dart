2d818ed918f7b4078d2277fe52c3e50559bcf6ed
Hari25483 <haricodmw@gmail.com>
2022-06-03 23:54:13 +0530

    Update constants.dart

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -11,3 +11,12 @@ const LabelTextStyle = TextStyle(
 );
 
 const knumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w800);
+
+const kTitleTextStyle = TextStyle(fontSize: 50.00, fontWeight: FontWeight.bold);
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontSize: 22.0, fontWeight: FontWeight.bold);
+
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+
+const kBodyTextStyle = TextStyle(fontSize: 22.0);
