26887321554d0e4a61e6951c7b1666790d505dad
Hari25483 <haricodmw@gmail.com>
2022-06-06 23:49:02 +0530

    Update input_page.dart

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -119,6 +119,53 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           Expanded(
+            child: Reusablewidget(
+              cardChild: Column(
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: LabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(height.toString(), style: knumberTextStyle),
+                      Text('cm', style: LabelTextStyle)
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.00),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.00),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                        print(newValue);
+                      },
+                    ),
+                  )
+                ],
+              ),
+              colour: kactiveWidgetColor,
+            ),
+          ),
+          Expanded(
             child: Row(
               children: [
                 Expanded(
