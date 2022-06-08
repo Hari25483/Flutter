fc28b60c9fd838995e2f40c59f38774fc399e5c7
Hari25483 <haricodmw@gmail.com>
2022-06-08 13:01:47 +0530

    Update loading_screen.dart

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,9 +1,9 @@
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/networking.dart';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:clima/services/location.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,19 +18,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Location location = new Location();
-    await location.get_current_Location();
-    longtitude = location.longtitude;
-    latitude = location.latitude;
-
-    NetworkHelper networkHelper = NetworkHelper(
-        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longtitude&appid=9a35db82a1d6216204de70f210a9fa27&units=metric");
-
-    var Weatherdata = await networkHelper.getData();
+    var WeatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
-        locationWeather: Weatherdata,
+        locationWeather: WeatherData,
       );
     }));
   }
