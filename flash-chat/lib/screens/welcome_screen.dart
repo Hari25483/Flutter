9752b7471c2adc057ff0b859cc45ad37a862ff92
Hari25483 <haricodmw@gmail.com>
2022-06-09 23:08:53 +0530

    Update welcome_screen.dart

--- flash-chat/lib/screens/welcome_screen.dart
+++ flash-chat/lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static String id = 'Welcome_Screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
