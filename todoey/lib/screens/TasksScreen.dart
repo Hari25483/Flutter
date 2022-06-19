00c6b5878b5670215feda45ab4e6db6e1e1dfd27
Hari25483 <haricodmw@gmail.com>
2022-06-19 14:06:36 +0530

    Create Todo app and design first page

---
+++ todoey/lib/screens/TasksScreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/task_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30,
+                child: Icon(
+                  Icons.list,
+                  size: 30,
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Todoey',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            child: TasksList(),
+          ),
+        ),
+      ]),
+    );
+  }
+}
